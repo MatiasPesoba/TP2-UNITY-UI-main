c3cc8f83eda0f2f83337a47c47bc126f54af5acf
MatiasPesoba <101577223+MatiasPesoba@users.noreply.github.com>
2022-04-25 15:05:26 -0300

    Initial commit

---
+++ Assets/Scripts/MayoríadeEdad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MayoríadeEdad : MonoBehaviour
+{
+    public int edadusuario;
+    public Text mitexto;
+    void Start()
+    {
+        if (edadusuario >= 18)
+        {
+            Debug.Log("Usted es mayor de edad");
+            mitexto.text = "Usted es mayor de edad";
+        }
+        else
+        {
+            Debug.Log("Usted no es mayor de edad");
+            mitexto.text = "Usted no es mayor de edad";
+
+        }
+    }
+}
